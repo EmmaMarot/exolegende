1733f9a3d2af1b8a02e32c1c1946d6cbe1d3ba3f
lemairelouis <louis.lemaire@uliege.be>
2025-03-08 22:03:13 +0100

    add getNearestEnemyPos()

---
+++ src/getNearestEnemyPos.cpp
@@ -0,0 +1,42 @@
+#include <cfloat>
+#include "getNearestEnemyPos.h"
+#include <cmath>  // Ensure sqrt function is available
+
+Position *getNearestEnemyPos(Gladiator *gladiator) {
+
+    RobotList robotList = gladiator->game->getPlayingRobotsId();
+    byte *IDs = robotList.ids;
+
+    RobotData mydata = gladiator->robot->getData();
+    byte myTeamId = mydata.teamId;
+    Position myPos = mydata.position;
+
+    Position *pos = NULL;
+    float minDist = FLT_MAX;  // Initialize to a large value
+
+    // Iterate over robot IDs to find the nearest enemy
+    for (int i = 0; i < 4; i++)  // Assuming 'count' gives the number of robots
+    {
+        if (IDs[i] != mydata.id)  // Ensure we're not checking ourselves
+        {
+            RobotData robotData = gladiator->game->getOtherRobotData(IDs[i]);
+            byte robotTeamID = robotData.teamId;
+
+            if (robotTeamID != myTeamId)  // Check if it's an enemy
+            {
+                Position *pos_temp = &robotData.position;
+
+                float dist_x = pos_temp->x - myPos.x;
+                float dist_y = pos_temp->y - myPos.y;
+                float dist_tmp = sqrt(dist_x * dist_x + dist_y * dist_y);
+
+                if (dist_tmp < minDist) {
+                    minDist = dist_tmp;
+                    pos = pos_temp;
+                }
+            }
+        }
+    }
+
+    return pos;  // Ensure function returns a value
+}
