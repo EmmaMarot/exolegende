47845e9d24b42d269b829c7a01de3ff2eda27b1f
lemairelouis <louis.lemaire@uliege.be>
2025-03-08 23:09:52 +0100

    Fix getNeartesEnemyPos()

--- src/getNearestEnemyPos.cpp
+++ src/getNearestEnemyPos.cpp
@@ -17,22 +17,23 @@ Position *getNearestEnemyPos(Gladiator *gladiator) {
     // Iterate over robot IDs to find the nearest enemy
     for (int i = 0; i < 4; i++)  // Assuming 'count' gives the number of robots
     {
-        if (IDs[i] != mydata.id)  // Ensure we're not checking ourselves
+        uint8_t current_id = (uint8_t) IDs[i];
+        if (current_id && current_id != mydata.id)  // Ensure we're not checking ourselves
         {
             RobotData robotData = gladiator->game->getOtherRobotData(IDs[i]);
             byte robotTeamID = robotData.teamId;
 
             if (robotTeamID != myTeamId)  // Check if it's an enemy
             {
-                Position *pos_temp = &robotData.position;
+                Position pos_temp = robotData.position;
 
-                float dist_x = pos_temp->x - myPos.x;
-                float dist_y = pos_temp->y - myPos.y;
+                float dist_x = pos_temp.x - myPos.x;
+                float dist_y = pos_temp.y - myPos.y;
                 float dist_tmp = sqrt(dist_x * dist_x + dist_y * dist_y);
 
                 if (dist_tmp < minDist) {
                     minDist = dist_tmp;
-                    pos = pos_temp;
+                    pos = &pos_temp;
                 }
             }
         }
